a2b467e077378a51ab008911b032fbc76f31aebc
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2025-03-28 01:48:18 +0530

    counter using closure

--- index.js
+++ index.js
@@ -6,5 +6,31 @@ function first(name){
 
 }
 
-p=first("ani");
-p();
+// p=first("ani");
+// p();
+
+function adderFix(num){
+  function adding(b){
+    console.log(num+b);
+  }
+  return adding;
+}
+
+let addT = adderFix(10);
+addT(15) ;
+/// counter 
+countShow = document.getElementById('show');
+cBtn = document.getElementById('btn');
+
+function countI(){
+  let c = 0;
+  function counter(){
+    c++;
+    console.log(c);
+    countShow.innerText = c;
+  }
+  return counter;
+}
+let p = countI() ;
+
+cBtn.addEventListener("click",p);
